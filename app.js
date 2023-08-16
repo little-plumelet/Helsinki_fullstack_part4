bc715dd92fe5858ba290f94f377e54b32c7f3914
Anastasiia Perova <perovaa.24@gmail.com>
2023-08-16 12:06:49 +0400

    start task 4.8 - change structure of the app & add supertest

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const Blog = require('./models/blog')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
